7298588f325448aefdbf2f23775bcc7566194946
Alexey Leshchenko <60138889+leshchenko1979@users.noreply.github.com>
2020-10-15 01:45:10 +0300

    собственная реализация php.http_build_query (#73)

--- fast_bitrix24/test_.py
+++ fast_bitrix24/test_.py
@@ -1,3 +1,4 @@
+from .utils import http_build_query
 import os
 
 import pytest
@@ -86,7 +87,7 @@ class TestLongRequests:
         b.get_by_ID('crm.lead.delete', [lead_no])
         
         
-class TestEmbeddedListsInParams:
+class TestParamsEncoding:
     
     def test_mobile_phone(self, get_test):
         b = get_test
@@ -105,3 +106,80 @@ class TestEmbeddedListsInParams:
             assert lead['PHONE'][0]['VALUE_TYPE'] == 'MOBILE'
         finally:
             b.get_by_ID('crm.lead.delete', [lead_no])
+            
+
+    def test_filter_not_equal(self, create_100_leads):
+        b = create_100_leads
+
+        result = b.get_all('crm.lead.list', {
+            'FILTER': {
+                '!STATUS_ID': 'NEW'
+            }
+        })
+        assert not result
+            
+        result = b.get_all('crm.lead.list', {
+            'FILTER': {
+                '!STATUS_ID': 'CLOSED'
+            }
+        })
+        assert result
+            
+        result = b.get_all('crm.lead.list', {
+            'FILTER': {
+                '<>STATUS_ID': 'NEW'
+            }
+        })
+        assert result
+        
+        result = b.get_all('crm.lead.list', {
+            'FILTER': {
+                '<>STATUS_ID': 'CLOSED'
+            }
+        })
+        assert result
+
+
+class TestHttpBuildQuery:
+    
+    def test_original(self):
+        assert http_build_query ({"alpha": "bravo"}) == "alpha=bravo&"
+
+        test = http_build_query({"charlie": ["delta", "echo", "foxtrot"]})
+        assert "charlie[0]=delta" in test
+        assert "charlie[1]=echo" in test
+        assert "charlie[2]=foxtrot" in test
+
+        test = http_build_query({
+            "golf": [
+                "hotel",
+                {"india": "juliet", "kilo": ["lima", "mike"]},
+                "november", "oscar"
+            ]
+        })
+        assert "golf[0]=hotel" in test
+        assert "golf[1][india]=juliet" in test
+        assert "golf[1][kilo][0]=lima" in test
+        assert "golf[1][kilo][1]=mike" in test
+        assert "golf[2]=november" in test
+        assert "golf[3]=oscar" in test
+
+
+    def test_new(self):
+        d = {
+            'FILTER': {
+                'STATUS_ID': 'CLOSED'
+            }
+        }
+        
+        test = http_build_query(d)
+        assert test == 'FILTER[STATUS_ID]=CLOSED&' 
+        
+        d = {
+            'FILTER': {
+                '!STATUS_ID': 'CLOSED'
+            }
+        }
+        
+        test = http_build_query(d)
+        assert test == 'FILTER[%21STATUS_ID]=CLOSED&' 
