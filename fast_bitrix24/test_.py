603087a2ec0e1439e2a156acc52b9430e6e43e11
leshchenko1979 <60138889+leshchenko1979@users.noreply.github.com>
2020-10-14 17:31:42 +0300

    - обязательное удаление лидов + параллельные тесты

--- fast_bitrix24/test_.py
+++ fast_bitrix24/test_.py
@@ -16,24 +16,36 @@ def get_test():
     
 @pytest.fixture(scope='session')
 def delete_all_leads(get_test):
+    '''Стирает все лиды.'''
+    # Вызывает проблемы при параллельном запуске тестов 
+    # в нескольких контейнерах, поэтому лучше использовать
+    # в крайних случаях. 
     b = get_test
     leads = b.get_all('crm.lead.list', {'select': ['ID']})
     b.get_by_ID('crm.lead.delete', [l['ID'] for l in leads])
     
     
 @pytest.fixture(scope='session')
-def create_1000_leads(get_test, delete_all_leads):
+def create_100_leads(get_test, delete_all_leads):
     b = get_test
     
-    delete_all_leads
+    # Подчистить тестовый аккаунт от лишних сущностей, 
+    # созданных при неудачных тестах, чтобы не было блокировки 
+    # аккаунта при создании более 1000 сущностей.
+    # Скорее всего, вызовет проблемы в параллельно 
+    # запущенных тестах.
+    total_leads = len(b.get_all('crm.lead.list'))
+    if total_leads > 500:
+        delete_all_leads
+
     with slow(1.2):
         lead_nos = b.call('crm.lead.add', [{
             'fields': {
                 'NAME': f'Customer #{n}',
             }
-        } for n in range(1000)])
+        } for n in range(100)])
     
-    yield True
+    yield b
     
     b.get_by_ID('crm.lead.delete', lead_nos)
 
@@ -51,18 +63,16 @@ class TestBasic:
         b.get_by_ID('crm.lead.delete', [lead_no])
         
         
-    def test_simple_get_all(self, get_test, delete_all_leads, create_1000_leads):
-        b = get_test
-        create_1000_leads
+    def test_simple_get_all(self, create_100_leads):
+        b = create_100_leads
         
-        leads = b.get_all('crm.lead.list')
+        resulting = len(b.get_all('crm.lead.list'))
         
-        assert len(leads) == 1000
+        assert resulting >= 100
         
         
-    def test_get_all_params(self, get_test, create_1000_leads):
-        b = get_test
-        create_1000_leads
+    def test_get_all_params(self, create_100_leads):
+        b = create_100_leads
         
         fields = ['ID', 'NAME']
         
