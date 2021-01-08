07da8559725689704d995f0cc27b4d661fefe455
leshchenko1979 <60138889+leshchenko1979@users.noreply.github.com>
2021-01-08 21:46:55 +0300

    bugfix

--- fast_bitrix24/tests/test_async.py
+++ fast_bitrix24/tests/test_async.py
@@ -52,7 +52,7 @@ async def assert_time_acquire(bitrix, acquire_amount, time_expected):
     t1 = monotonic()
 
     for _ in range(acquire_amount):
-        await bitrix.srh._acquire()
+        await bitrix.srh.acquire()
 
     t2 = monotonic()
 
