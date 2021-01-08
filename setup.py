d918170b521a62bbbe095533a3f858a20fbf5d03
leshchenko1979 <60138889+leshchenko1979@users.noreply.github.com>
2021-01-08 23:53:32 +0300

    ver bump

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('requirements.txt', 'r', encoding='utf-8') as fh:
 
 setuptools.setup(
     name="fast_bitrix24",
-    version="1.0.2",
+    version="1.1.0",
     author="Alexey Leshchenko",
     author_email="leshchenko@gmail.com",
     description='API wrapper для быстрого получения данных от Битрикс24 через '
