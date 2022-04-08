9e4c5016331bd4d6d43c68abb23eda1bea2ddcad
Alexey Leshchenko <leshchenko@gmail.com>
2022-04-08 22:23:51 +0300

    Use black and minor refactorings

--- fast_bitrix24/correct_asyncio.py
+++ fast_bitrix24/correct_asyncio.py
@@ -1,4 +1,9 @@
-import sys, asyncio
+import asyncio
+import sys
 
-if sys.version_info[0] == 3 and sys.version_info[1] >= 8 and sys.platform.startswith('win'):
+if (
+    sys.version_info[0] == 3
+    and sys.version_info[1] >= 8
+    and sys.platform.startswith("win")
+):
     asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
--- fast_bitrix24/mult_request.py
+++ fast_bitrix24/mult_request.py
@@ -10,9 +10,9 @@ from .utils import http_build_query
 
 
 class MultipleServerRequestHandler:
-
-    def __init__(self, bitrix, method, item_list,
-                 real_len=None, real_start=0, mute=False):
+    def __init__(
+        self, bitrix, method, item_list, real_len=None, real_start=0, mute=False
+    ):
         self.bitrix = bitrix
         self.srh: ServerRequestHandler = bitrix.srh
         self.method = method
@@ -26,48 +26,50 @@ class MultipleServerRequestHandler:
         self.tasks = set()
 
     def generate_a_task(self):
-        '''Объединяем элементы item_list в батчи и по одной создаем и
-        возвращаем задачи asyncio с запросами к серверу для каждого батча.'''
+        """Объединяем элементы item_list в батчи и по одной создаем и
+        возвращаем задачи asyncio с запросами к серверу для каждого батча."""
 
-        batches = ({
-            'halt': 0,
-            'cmd': {
-                self.batch_command_label(i, item):
-                f'{self.method}?{http_build_query(item)}'
-                for i, item in enumerate(next_batch)
-            }}
-            for next_batch in chunked(self.item_list, BITRIX_MAX_BATCH_SIZE))
+        batches = (
+            {
+                "halt": 0,
+                "cmd": {
+                    self.batch_command_label(
+                        i, item
+                    ): f"{self.method}?{http_build_query(item)}"
+                    for i, item in enumerate(next_batch)
+                },
+            }
+            for next_batch in chunked(self.item_list, BITRIX_MAX_BATCH_SIZE)
+        )
 
         for batch in batches:
-            yield ensure_future(self.srh.single_request('batch', batch))
+            yield ensure_future(self.srh.single_request("batch", batch))
 
     def batch_command_label(self, i, item):
-        return f'cmd{i}'
+        return f"cmd{i}"
 
     async def run(self):
         self.top_up_tasks()
 
         with self.get_pbar() as pbar:
             while self.tasks:
-                done, self.tasks = await wait(self.tasks,
-                                              return_when=FIRST_COMPLETED)
+                done, self.tasks = await wait(self.tasks, return_when=FIRST_COMPLETED)
                 extracted_len = self.process_done_tasks(done)
                 pbar.update(extracted_len)
                 self.top_up_tasks()
 
-    #            self.pbar.set_postfix({
-    #                'max. requests': self.srh.mcr_cur_limit,
-    #                'requests': self.srh.concurrent_requests,
-    #                'tasks': len(self.tasks)})
+        #            self.pbar.set_postfix({
+        #                'max. requests': self.srh.mcr_cur_limit,
+        #                'requests': self.srh.concurrent_requests,
+        #                'tasks': len(self.tasks)})
 
         return self.results
 
     def top_up_tasks(self):
-        '''Добавляем в self.tasks столько задач, сколько свободных слотов для
-        запросов есть сейчас в self.srh.'''
+        """Добавляем в self.tasks столько задач, сколько свободных слотов для
+        запросов есть сейчас в self.srh."""
 
-        to_add = max(
-            int(self.srh.mcr_cur_limit) - self.srh.concurrent_requests, 0)
+        to_add = max(int(self.srh.mcr_cur_limit) - self.srh.concurrent_requests, 0)
         for _ in range(to_add):
             try:
                 self.tasks.add(next(self.task_iterator))
@@ -75,21 +77,20 @@ class MultipleServerRequestHandler:
                 break
 
     def process_done_tasks(self, done):
-        '''Извлечь результаты из списка законченных задач
-        и вернуть кол-во извлеченных элементов.'''
+        """Извлечь результаты из списка законченных задач
+        и вернуть кол-во извлеченных элементов."""
 
         extracted_len = 0
         for done_task in done:
             batch_response = done_task.result()
             unwrapped_result = ServerResponse(batch_response.result).result
-            extracted_len += self.extract_result_from_batch_response(
-                unwrapped_result)
+            extracted_len += self.extract_result_from_batch_response(unwrapped_result)
 
         return extracted_len
 
     def extract_result_from_batch_response(self, unwrapped_result):
-        '''Добавляет `unwrapped_result` в `self.results` и возвращает
-        длину добавленного списка результатов'''
+        """Добавляет `unwrapped_result` в `self.results` и возвращает
+        длину добавленного списка результатов"""
 
         if not unwrapped_result:
             return 0
@@ -101,17 +102,18 @@ class MultipleServerRequestHandler:
         return len(result_list)
 
     def get_pbar(self):
-        '''Возвращает прогресс бар `tqdm()` или пустышку,
-        если `self.bitrix.verbose is False`.'''
+        """Возвращает прогресс бар `tqdm()` или пустышку,
+        если `self.bitrix.verbose is False`."""
 
         if self.bitrix.verbose and not self.mute:
-            return tqdm(total=(self.real_len or len(self.item_list)),
-                        initial=self.real_start)
+            return tqdm(
+                total=(self.real_len or len(self.item_list)), initial=self.real_start
+            )
         else:
             return MutePBar()
 
 
-class MutePBar():
+class MutePBar:
     def __enter__(self):
         return self
 
@@ -123,7 +125,6 @@ class MutePBar():
 
 
 class MultipleServerRequestHandlerPreserveIDs(MultipleServerRequestHandler):
-
     def __init__(self, bitrix, method, item_list, ID_field):
         super().__init__(bitrix, method, item_list)
         self.ID_field = ID_field
