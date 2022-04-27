f53d802a543855cdaf373d961bb70e327ffd2038
Alexey Leshchenko <leshchenko@gmail.com>
2022-04-28 00:10:22 +0300

    Remove a contract from `make_first_request()` results

--- fast_bitrix24/user_request.py
+++ fast_bitrix24/user_request.py
@@ -132,7 +132,6 @@ class GetAllUserRequest(UserRequestAbstract):
         else:
             self.params = order_clause
 
-    @icontract.ensure(lambda self: isinstance(self.results, list))
     async def make_first_request(self):
         self.first_response = await self.srh.single_request(self.method, self.params)
         self.total = self.first_response.total
