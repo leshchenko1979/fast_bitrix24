1df5be11563a7cdb16cb5770fb31be7f3f2c9ef0
Alexey Leshchenko <leshchenko@gmail.com>
2023-11-20 18:16:26 +0300

    Добавить в лог тип IPython

--- fast_bitrix24/logger.py
+++ fast_bitrix24/logger.py
@@ -1,12 +1,19 @@
 from logging import DEBUG, NullHandler, getLogger
-from .__version__ import __version__
-
+from __init__ import __version__
 
 logger = getLogger("fast_bitrix24")
 logger.setLevel(DEBUG)
 logger.addHandler(NullHandler())
 
 logger.debug(f"fast_bitrix24 version: {__version__}")
+
+try:
+    from IPython import get_ipython
+
+    logger.debug(f"IPython: {get_ipython()}")
+except ImportError:
+    logger.debug("No IPython found")
+
 
 def log(func):
     async def wrapper(*args, **kwargs):
