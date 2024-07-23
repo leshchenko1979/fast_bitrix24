fde51d0ba71c76b7bb7053275fd421e0c762dfbc
Alexey Leshchenko <leshchenko@gmail.com>
2024-07-23 22:01:14 +0300

    Fix if params is None

--- fast_bitrix24/user_request.py
+++ fast_bitrix24/user_request.py
@@ -52,7 +52,7 @@ class UserRequestAbstract:
 
         # st_params будет использоваться для проверки параметров,
         # но на сервер должны уходить параметры без изменения регистра
-        self.params = params.copy()
+        self.params = params.copy() if params else {}
         self.st_params = self.standardized_params(params)
 
         self.mute = mute
